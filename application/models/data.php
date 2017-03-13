64bac3d129e9c8d925e341df7ff14a05c0eb4b67
Nikola Donchev <ndon4ev@gmail.com>
2017-03-13 16:29:11 +0000

    Update and fix username

--- application/models/data.php
+++ application/models/data.php
@@ -1,15 +1,40 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Site_data extends CI_Model {
+class Data extends CI_Model {
 
     public function __construct() {
         $this->load->database();
     }
 
     public function get_campaigns() {
-        $query = $this->db->query('SELECT * FROM `aidtrack_campaigns`');
+        $query = $this->db->query('SELECT * FROM aidtrack_campaigns');
         return $query->result_array();
+    }
+
+     public function add_campaign($campaign_name) {
+        $query = "INSERT INTO aidtrack_campaigns (id, campaign_name, created_by) VALUES ('', ?, '1')";
+        $this->db->query($query, array($campaign_name));
+    }
+
+    public function get_shipments() {
+        $query = $this->db->query('SELECT * FROM aidtrack_shipments');
+        return $query->result_array();
+    }
+
+    public function add_shipment($shipment_title, $camp_id) {
+        $query = "INSERT INTO aidtrack_shipments (id, shipment_title, campaign_id) VALUES ('', ?, ?)";
+        $this->db->query($query, array($shipment_title, $camp_id));
+    }
+
+    public function get_products() {
+        $query = $this->db->query('SELECT * FROM aidtrack_products');
+        return $query->result_array();
+    }
+    
+    public function add_product($product_name) {
+        $query = "INSERT INTO aidtrack_products (id, product_name) VALUES ('', ?)";
+        $this->db->query($query, array($product_name));
     }
 
 }
