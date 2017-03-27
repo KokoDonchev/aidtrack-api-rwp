8b3195c2d204146f990143d2cfc5d676c495b654
Nikola Donchev <ndon4ev@gmail.com>
2017-03-27 11:59:16 +0100

    updated homepage

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,13 +20,6 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		// $this->load->view('welcome_message');
-
-		$this->load->database();
-
-		$query = $this->db->query('SELECT * FROM `aidtrack_campaigns`');
-
-		var_dump($query->result());
-
+		$this->load->view('welcome_message');
 	}
 }
